ff5885530c4097a878252627b8ca3322862e14d1
Pallavi-Sinha-12 <pallavisinha95829@gmail.com>
2024-05-21 13:29:10 +0530

    Refactoring

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 def run():
     # inputs = {"topic_details" : "Explain basics of FastAPI framework."} # Topic 1
-    inputs = {"topic_details" : "Explain Python decorators."} # Topic 2
+    # inputs = {"topic_details" : "Explain Python decorators."} # Topic 2
+    inputs = {"topic_details" : "Explain how to quickly prototype using Streamlit."} # Topic 3
     BloggingCrew().blogging_crew().kickoff(inputs = inputs)
 
 if __name__ == "__main__":
